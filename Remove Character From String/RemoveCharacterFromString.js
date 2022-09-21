36c9b80464c0cadd63373abd049818374b784229
Hokageto <didorhristozov@gmail.com>
2022-09-21 22:35:32 +0300

    Remove Character From String task

---
+++ Remove Character From String/RemoveCharacterFromString.js
@@ -0,0 +1,7 @@
+// *Challenge 4
+// Write a JavaScript program to remove a character at the specified position of a given string and return the new string.
+
+// console.log(removeCharacter('Example', 0)) => should log xample
+
+let sentence = "This is a short sentence.";
+console.log(sentence.replace("sentence", "entence"));
