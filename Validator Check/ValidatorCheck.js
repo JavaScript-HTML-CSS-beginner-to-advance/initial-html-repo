9e6df2ce6b34096e0fd24aa2815b03b83543b29d
Hokageto <didorhristozov@gmail.com>
2022-09-21 22:49:38 +0300

    Validator Check Task

---
+++ Validator Check/ValidatorCheck.js
@@ -0,0 +1,14 @@
+// *Challenge 5
+// Write a JavaScript program to check whether a string starts with 'Java' and false otherwise.
+
+// function checkStringStart(str) {
+
+// }
+
+// console.log(checkStringStart("JavaScript"));
+// console.log(checkStringStart("Java"));
+// console.log(checkStringStart("Python"));
+
+let first = "This sentence ends with Javascript.";
+let second = "Javascript is the starting word in this sentence.";
+console.log(second.startsWith("Javascript"));
